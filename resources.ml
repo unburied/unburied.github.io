e445ade6355e7f4c62d1e652ae45fe76d8877fc2
David Martinez <49927967+unburied@users.noreply.github.com>
2019-05-14 21:40:04 -0400

    made it work..i think

--- resources.ml
+++ resources.ml
@@ -1,8 +1,8 @@
-## Resources
-
-
-### These are some of my favorite resources I have come accross in my DS journey:
-
+---
+layout: page
+title: Resources
+subtitle: These are some of my favorite resources I have come accross in my DS journey:
+---
 ####Pandas
 *
 *
